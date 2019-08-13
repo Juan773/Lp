c7ef535e27039108362097ac14dfb337622109cf
Juan Hus Quezada <juanhusquezada@MacBook-Air-de-Juan.local>
2019-08-13 18:33:55 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UnoComponent } from './uno/uno.component';
+import { DosComponent } from './dos/dos.component';
+import { TresComponent } from './tres/tres.component';
+import { CuatroComponent } from './cuatro/cuatro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'uno', component: UnoComponent},
+{path:'dos', component: DosComponent},
+{path:'tres', component: TresComponent},
+{path:'cuatro', component: CuatroComponent},
+]
+;
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const rountingComponents = [UnoComponent,DosComponent,TresComponent,CuatroComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnoComponent } from './uno/uno.component';
+import { DosComponent } from './dos/dos.component';
+import { TresComponent } from './tres/tres.component';
+import { CuatroComponent } from './cuatro/cuatro.component';
+import { CincoComponent } from './cinco/cinco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnoComponent,
+    DosComponent,
+    TresComponent,
+    CuatroComponent,
+    CincoComponent
   ],
   imports: [
     BrowserModule,
